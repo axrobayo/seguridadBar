f5ec26ccef04a602d19e5ff4f81ba26ce3ab1304
ldlignia23 <ldlignia@espe.edu.ec>
2022-06-14 11:04:12 -0500

    index1

--- index.php
+++ index.php
@@ -10,6 +10,14 @@
     <div class="error">
         <span>Bar ESPEL</span>
     </div>
-
+    <div class="main" >
+        <form action="" id="form">
+            <input type="text" name="usuario" placeholder="Usuario" />
+            <input type="text" name="pass" placeholder="Contraseña" />
+            <input type="submit" name="usuario" placeholder="Inicar Sesión" />
+        </form>
+    </div>
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 </html>
