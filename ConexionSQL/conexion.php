68e33b72f6738d9512aaddd639c212064942574a
rvpichucho <63176305+rvpichucho@users.noreply.github.com>
2022-06-14 16:31:33 -0500

    Diseños

--- ConexionSQL/conexion.php
+++ ConexionSQL/conexion.php
@@ -6,21 +6,5 @@ $password = "";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
 
-$result = mysqli_query($conn, "SELECT * FROM bar");
-
-mysqli_data_seek ($result, 0);
-
-$extraido= mysqli_fetch_array($result);
-
-echo "- Id: ".$extraido['bar_id']."<br/>";
-
-echo "- Nombre: ".$extraido['bar_nombre']."<br/>";
-mysqli_free_result($result);
-
-echo "Connected successfully";
-mysqli_close($conn);
 ?>
